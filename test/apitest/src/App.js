5990203d201f71624e9f805e1ace852c4e6f5b20
wlals <jiminelp@gmail.com>
2023-09-20 18:41:57 +0900

    📝:: React learn proxy

--- test/apitest/src/App.js
+++ test/apitest/src/App.js
@@ -3,28 +3,46 @@ import axios from "axios";
 import React, { useState } from "react";
 
 const App = () => {
+  const [num, setNum] = useState(0);
   const [responseData, setResponseData] = useState(null);
-
+  const plusNum = () => {
+    setNum(num + 10);
+  };
   const fetchData = async () => {
     try {
-      const response = await axios.post("http://localhost:80/hina", {
+      const response = await axios.post("/hina", {
         number: "70",
         hina: "so cute",
         hapycb: "so sexy",
       });
       console.log(response.data);
-      setResponseData(responseData);
+      setResponseData(response.data);
+      setNum(response.data.number);
+      if (responseData) {
+        const maps = responseData.map;
+        console.log(maps);
+      }
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
+
   return (
     <>
       <button onClick={fetchData}>Fetch Data</button>
       {responseData && (
         <div>
+          <button
+            onClick={() => {
+              plusNum();
+            }}
+          >
+            +10
+          </button>
           <h2>Response Data:</h2>
           <pre>{JSON.stringify(responseData, null, 2)}</pre>
+          <p>{num}</p>
+          <p>{responseData.hina}</p>
         </div>
       )}
     </>
